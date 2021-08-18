310fac1ef31e21619e1e147ea481846cb0da3024
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-18 10:00:27 -0400

    Added registry information, added function taht calculates BUji as well as attempted to start function that calculates hessian

--- src/Uwham.cpp
+++ src/Uwham.cpp
@@ -1,1 +1,73 @@
 #include "Uwham.h"
+
+namespace WhamRegistry
+{
+    registry<Uwham> registerUwham("Uwham");
+}
+
+Uwham::Uwham(const ParameterPack& input)
+:Wham(input)
+{
+    N_.resize(VectorTimeSeries_.size());
+    for (int i=0;i<VectorTimeSeries_.size();i++)
+    {
+        xji_.insert(xji_.end(),VectorTimeSeries_[i].begin(), VectorTimeSeries_[i].end());
+        N_[i] = VectorTimeSeries_[i].getSize(); 
+
+        Ntot_ += N_[i];
+    }
+
+    auto biases = input.findParamPacks("bias", ParameterPack::KeyType::Required);
+    ASSERT((biases.size() == VectorTimeSeries_.size()), "The number of time series does not match the number of biases.");
+
+    for (int i=0;i<biases.size();i++)
+    {
+        std::string biastype = "simplebias";
+        biases[i] -> ReadString("type", ParameterPack::KeyType::Optional, biastype);
+        Biasptr b = Biasptr(BiasRegistry::Factory::instance().create(biastype, *biases[i]));
+
+        Biases_.push_back(std::move(b));
+    }
+
+    BUji_.resize(biases.size(), xji_.size());
+    for (int i=0;i<biases.size();i++)
+    {
+        for (int j=0;j<xji_.size();j++)
+        {
+            Real val = Biases_[i]->calculate(xji_[j]); 
+            BUji_(i,j) = val;
+        }
+    }
+}
+
+void Uwham::calculate()
+{
+
+}
+
+Matrix<Uwham::Real> Uwham::Hessian(const Matrix<Real>& BUji, const std::vector<Real>& fi, const std::vector<Real>& N)
+{
+    int Ntot = 0;
+    for (int i=0;i<N.size();i++)
+    {
+        Ntot += N[i];
+    }
+
+    Matrix<Real> Hessian(BUji_.getNR(), BUji_.getNR());
+
+    std::vector<Real> lnwji;
+    lnwji.resize(BUji_.getNC());
+
+    for (int i=0;i<lnwji.size();i++)
+    {
+        std::vector<Real> column;
+        column.resize(BUji_.getNR());
+        for (int j=0;j<BUji_.getNR();j++)
+        {
+            column[j] = fi[j]-1.0*BUji_(j,i);
+        }
+
+        Real val = WhamTools::LogSumExp(column, N);
+        lnwji[i] = val;
+    }
+}
--- src/Uwham.h
+++ src/Uwham.h
@@ -1,15 +1,30 @@
+#pragma once
 #include "Wham.h"
 #include "tools/InputParser.h"
+#include "Array.h"
+#include "Bias.h"
 
 #include <vector>
 #include <array>
+#include <memory>
 
 class Uwham:public Wham
 {
     public:
+        using Biasptr = std::unique_ptr<Bias>;
+
         Uwham(const ParameterPack& pack);
         virtual ~Uwham(){};
+        Matrix<Real> Hessian(const Matrix<Real>& BUji, const std::vector<Real>& fi, const std::vector<Real>& N);
 
         virtual void calculate();
+
     private:
+        Matrix<Real> BUji_;
+        std::vector<Real> fi_;
+        std::vector<std::vector<Real>> xji_;
+
+        std::vector<Biasptr> Biases_;
+
+        int Ntot_;
 };
