6609f13cc882de65e87ccc0da96bcac063ac09e0
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-18 10:03:24 -0400

    Changed main such that we are now actually instantiating wham obj

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include "src/TimeSeries.h"
 #include "tools/CommonTypes.h"
 #include "tools/InputParser.h"
+#include "src/Wham.h"
 
 #include <vector>
 #include <iostream>
+#include <string>
+#include <memory>
 
 int main(int argc, char** argv)
 {
     using Real = CommonTypes::Real;
+    using Whamptr = std::unique_ptr<Wham>;
 
     std::string fname(argv[1]);
 
@@ -15,9 +19,11 @@ int main(int argc, char** argv)
     ParameterPack pack;
     ip.ParseFile(fname, pack);
 
-    auto tspack = pack.findParamPack("timeseries", ParameterPack::KeyType::Required);
+    auto WhamPack = pack.findParamPack("wham", ParameterPack::KeyType::Required);
 
-    TimeSeries ts(*tspack);
-
+    std::string WhamType;
+    WhamPack->ReadString("type", ParameterPack::KeyType::Required, WhamType);
+    Whamptr w = Whamptr(WhamRegistry::Factory::instance().create(WhamType, pack));
+ 
     return 0;
 }
