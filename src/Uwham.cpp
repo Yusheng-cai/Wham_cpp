2e6681bf3b609d29b7c732decf218ee9e0668565
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-18 23:33:13 -0400

    Added code for bins and outputting

--- src/Uwham.cpp
+++ src/Uwham.cpp
@@ -9,13 +9,24 @@ Uwham::Uwham(const ParameterPack& input)
 :Wham(input)
 {
     N_.resize(VectorTimeSeries_.size());
+    std::vector<int> dimensions_(VectorTimeSeries_.size());
+
     for (int i=0;i<VectorTimeSeries_.size();i++)
     {
         xi_.insert(xi_.end(),VectorTimeSeries_[i].begin(), VectorTimeSeries_[i].end());
         N_[i] = VectorTimeSeries_[i].getSize(); 
+        dimensions_[i] = VectorTimeSeries_[i].getDimension();
 
         Ntot_ += N_[i];
     }
+
+    for (int i=0;i<dimensions_.size()-1;i++)
+    {
+        ASSERT((dimensions_[i] == dimensions_[i+1]), "The dimension in the " << i << "th timeseries does not match with the " << i+1 << "th time series");
+    }
+
+    // record the dimensions of this Wham calculation
+    dimension_ = dimensions_[0];
 
     auto biases = input.findParamPacks("bias", ParameterPack::KeyType::Required);
     ASSERT((biases.size() == VectorTimeSeries_.size()), "The number of time series does not match the number of biases.");
@@ -29,31 +40,81 @@ Uwham::Uwham(const ParameterPack& input)
         Biases_.push_back(std::move(b));
     }
 
-    BUki_.resize(biases.size(), xi_.size());
-    for (int i=0;i<biases.size();i++)
+    BUki_.resize(Biases_.size(), xi_.size());
+    #pragma omp parallel for
+    for (int i=0;i<xi_.size();i++)
     {
-        for (int j=0;j<xi_.size();j++)
+        for (int j=0;j<Biases_.size();j++)
         {
-            Real val = Biases_[i]->calculate(xi_[j]); 
-            BUki_(i,j) = Biases_[i]->getBeta()*val;
+            Real val = Biases_[j]->calculate(xi_[i]); 
+            BUki_(j,i) = Biases_[j]->getBeta()*val;
         }
     }
 
     // Now read in what type of calculation are you letting Uwham do
     auto whamPack = input.findParamPack("wham", ParameterPack::KeyType::Required);
     initializeStrat(whamPack);
+
+    bool normRead = whamPack->ReadString("normalizationOutput", ParameterPack::KeyType::Optional, NormalizationFileOutput_);
+
+    if (normRead)
+    {
+        NormalizationFileofs_.open(NormalizationFileOutput_);
+        ASSERT((NormalizationFileofs_.is_open()), "The file with name " << NormalizationFileOutput_ << " is not opened.");
+    }
+
+    auto binPacks = whamPack -> findParamPacks("bins", ParameterPack::KeyType::Required);
+    initializeBins(binPacks);
+
+    ASSERT((binPacks.size() == dimension_), "The binning dimension is " << binPacks.size() << " while the dimension of the Wham is " << dimension_);
 }
+
+void Uwham::initializeBins(const std::vector<const ParameterPack*>& BinPacks)
+{
+    if (BinPacks.size() != 0)
+    {
+        for (int i=0;i<BinPacks.size();i++)
+        {
+            Bins_.push_back(Bin(*BinPacks[i])); 
+        }
+    }
+}
+
 
 void Uwham::initializeStrat(const ParameterPack* whampack)
 {
     std::string strat;
     whampack->ReadString("strategy", ParameterPack::KeyType::Required, strat);
 
-    UwhamStrategyInput input = {BUki_, N_};
+    UwhamStrategyInput input = {BUki_, N_, const_cast<ParameterPack&>(*whampack)};
     strat_ = stratptr(UwhamCalculationStrategyRegistry::Factory::instance().create(strat, input));
 }
 
 void Uwham::calculate()
 {
+    auto start = std::chrono::high_resolution_clock::now();
     strat_ -> calculate();
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto diff = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Calculation took " << diff.count() << std::endl;
+
+    const auto& lnwji = strat_ -> getlnwji_();
+}
+
+void Uwham::printOutput()
+{
+    if (NormalizationFileofs_.is_open())
+    {
+        int Nsim = BUki_.getNR();
+        NormalizationFileofs_ << "# normalization constants" << "\n";
+        for (int i=0;i<Nsim;i++)
+        {
+            NormalizationFileofs_ << strat_ -> getFk_()[i] << "\n";
+        }
+
+        NormalizationFileofs_.close();
+    }
+
 }
--- src/Uwham.h
+++ src/Uwham.h
@@ -3,11 +3,13 @@
 #include "tools/InputParser.h"
 #include "Array.h"
 #include "Bias.h"
+#include "Bin.h"
 #include "UwhamCalculationStrategy.h"
 
 #include <vector>
 #include <array>
 #include <memory>
+#include <iostream>
 
 class Uwham:public Wham
 {
@@ -18,12 +20,13 @@ class Uwham:public Wham
         Uwham(const ParameterPack& pack);
         virtual ~Uwham(){};
         void initializeStrat(const ParameterPack* pack);
+        void initializeBins(const std::vector<const ParameterPack*>& BinPacks);
 
         virtual void calculate();
+        virtual void printOutput() override;
 
     private:
         Matrix<Real> BUki_;
-        std::vector<Real> fk_;
         std::vector<std::vector<Real>> xi_;
 
         std::vector<Biasptr> Biases_;
@@ -31,4 +34,11 @@ class Uwham:public Wham
         int Ntot_;
 
         stratptr strat_;
+
+        std::string NormalizationFileOutput_;
+        std::ofstream NormalizationFileofs_;
+
+        int dimension_;
+
+        std::vector<Bin> Bins_;
 };
