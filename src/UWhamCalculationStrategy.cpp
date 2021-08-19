d1b49687840a09d2aa4b20bfd18e534cad61c7a1
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-18 23:35:18 -0400

    Minor changes

--- src/UWhamCalculationStrategy.cpp
+++ src/UWhamCalculationStrategy.cpp
@@ -4,5 +4,7 @@ UWhamCalculationStrategy::UWhamCalculationStrategy(UwhamStrategyInput& input)
 :BUki_(input.BUki_), N_(input.N)
 {
     fk_.resize(N_.size());
-    std::fill(fk_.begin(), fk_.end(), 0.0);
+    std::fill(fk_.begin(), fk_.end(), 1e-8);
+
+    lnwji_.resize(BUki_.getNC());
 }
