2717788b4c594e92b14dec4b5d43239078825797
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-19 14:36:41 -0400

    Added code in Bin to check whether or not a data point is in range

--- src/Bin.cpp
+++ src/Bin.cpp
@@ -4,6 +4,7 @@ Bin::Bin(const ParameterPack& pack)
 {
     pack.ReadArrayNumber("range", ParameterPack::KeyType::Required, range_);
     pack.ReadNumber("numbins", ParameterPack::KeyType::Required,numbins_);
+    pack.ReadNumber("dimension", ParameterPack::KeyType::Optional, dimension_);
 
     step_ = (range_[1] - range_[0])/numbins_;
 }
@@ -13,4 +14,21 @@ int Bin::findBin(Real x)
     int index = std::floor((x - range_[0])/step_);
 
     return index;
+}
+
+bool Bin::isInRange(Real data)
+{
+    if (data >= range_[0] && data <= range_[1])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Bin::Real Bin::getLocationOfBin(int binNum) const 
+{
+    return range_[0] + (binNum + 0.5) * step_;
 }
