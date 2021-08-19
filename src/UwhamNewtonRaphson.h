19020b51cdf40cba299e70d69b5b14beda4ad835
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-19 16:27:46 -0400

    Added Newton Raphson with wham

---
+++ src/UwhamNewtonRaphson.h
@@ -0,0 +1,11 @@
+#include "UWhamCalculationStrategy.h"
+
+#include <vector>
+#include <array>
+
+class UwhamNewtonRaphson : public UWhamCalculationStrategy
+{
+    public:
+        UwhamNewtonRaphson(UwhamStrategyInput& input);
+    private:
+};
