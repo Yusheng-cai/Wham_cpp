dd254b30a52b072e6c15704f4fc23db02899647b
Yusheng Cai <cys9741@seas.upenn.edu>
2021-11-01 21:12:06 -0400

    Added base class for timeseries operations

--- src/TSoperation.cpp
+++ src/TSoperation.cpp
@@ -3,5 +3,23 @@
 TSoperation::TSoperation(const TSInput& input)
 :VectorTS_(input.vectorTS_), pack_(input.pack_)
 {
+    pack_.ReadVectorString("outputs", ParameterPack::KeyType::Optional, VectorOutputNames_);
+    pack_.ReadVectorString("outputFile", ParameterPack::KeyType::Optional, VectorOutputFileNames_);
 
+    outputs_ = OutputFuncPtr(new Output());
+
+    for (int i=0;i<VectorOutputNames_.size();i++)
+    {
+        std::cout << VectorOutputNames_[i] << std::endl;
+    }
+    
+}
+
+void TSoperation::print()
+{
+    for (int i=0;i<VectorOutputNames_.size();i++)
+    {
+        std::cout << "name = " << VectorOutputNames_[i] << std::endl;
+        outputs_ -> getOutputFuncByName(VectorOutputNames_[i])(VectorOutputFileNames_[i]);
+    }
 }
--- src/TSoperation.h
+++ src/TSoperation.h
@@ -1,8 +1,10 @@
 #pragma once 
+
 #include "tools/OutputFunction.h"
 #include "TimeSeries.h"
 #include "tools/CommonTypes.h"
 #include "tools/InputParser.h"
+#include "tools/GenericFactory.h"
 
 #include <vector>
 #include <array>
@@ -11,24 +13,40 @@
 
 struct TSInput
 {
-    std::vector<TimeSeries>& vectorTS_;
+    using tsptr = std::shared_ptr<TimeSeries>;
     ParameterPack& pack_;
+    std::vector<tsptr>& vectorTS_;
 };
 
 class TSoperation
 {
     public:
+        using tsptr = std::shared_ptr<TimeSeries>;
         using OutputFuncPtr = std::unique_ptr<Output>;
 
         TSoperation(const TSInput& input);
 
-        virtual void calculate();
+        virtual void calculate() = 0;
         virtual void print();
     
-    private:
-        std::vector<TimeSeries>& VectorTS_;
+    protected:
+        std::vector<tsptr>& VectorTS_;
 
         OutputFuncPtr outputs_;
 
         ParameterPack& pack_;
+
+        std::vector<std::string> VectorOutputNames_;
+        std::vector<std::string> VectorOutputFileNames_;
 };
+
+namespace timeseriesOP
+{
+    using Base = TSoperation;
+    using Key  = std::string;
+
+    using Factory = GenericFactory<Base,Key,const TSInput&>;
+
+    template<typename D>
+    using registry = RegisterInFactory<Base, D, Key, const TSInput&>;
+}
