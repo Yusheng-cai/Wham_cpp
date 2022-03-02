3d159a879f4b7045de216ddf5227a1bca46e3357
Yusheng Cai <cys9741@seas.upenn.edu>
2022-03-01 19:05:13 -0500

    Minor changes

--- src/UWhamCalculationStrategy.cpp
+++ src/UWhamCalculationStrategy.cpp
@@ -6,4 +6,13 @@ UWhamCalculationStrategy::UWhamCalculationStrategy(UwhamStrategyInput& input)
     input.pack.ReadNumber("printevery", ParameterPack::KeyType::Optional,print_every_);
 
     lnwji_.resize(BUki_.getNC());
+
+    input.pack.ReadString("name", ParameterPack::KeyType::Required, name_);
+};
+
+void UWhamCalculationStrategy::setFk(std::vector<Real>& fk)
+{
+    ASSERT((fk.size() == BUki_.getNR()), "The set fk does not match the size");
+    fk_.clear();
+    fk_.insert(fk_.end(), fk.begin(), fk.end());
 }
