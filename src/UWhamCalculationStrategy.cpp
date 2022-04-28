7db55a43042fff50a54b310304e16782ef085db9
Yusheng Cai <cys9741@seas.upenn.edu>
2022-04-28 09:54:24 -0400

    UPdated Uwham calculation strat

--- src/UWhamCalculationStrategy.cpp
+++ src/UWhamCalculationStrategy.cpp
@@ -1,7 +1,7 @@
 #include "UwhamCalculationStrategy.h"
 
 UWhamCalculationStrategy::UWhamCalculationStrategy(UwhamStrategyInput& input)
-:BUki_(input.BUki_), N_(input.N), fk_(input.fk)
+:BUki_(input.BUki_), N_(input.N)
 {
     input.pack.ReadNumber("printevery", ParameterPack::KeyType::Optional,print_every_);
 
@@ -9,10 +9,3 @@ UWhamCalculationStrategy::UWhamCalculationStrategy(UwhamStrategyInput& input)
 
     input.pack.ReadString("name", ParameterPack::KeyType::Required, name_);
 };
-
-void UWhamCalculationStrategy::setFk(std::vector<Real>& fk)
-{
-    ASSERT((fk.size() == BUki_.getNR()), "The set fk does not match the size");
-    fk_.clear();
-    fk_.insert(fk_.end(), fk.begin(), fk.end());
-}
