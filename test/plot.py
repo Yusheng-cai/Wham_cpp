c0d8f1a7cb0b82c53cfe1fb04a87a4c550c7ae8f
Yusheng Cai <cys9741@seas.upenn.edu>
2022-04-29 17:18:22 -0400

    Added new test files as well as updated images

--- test/plot.py
+++ test/plot.py
@@ -9,14 +9,11 @@ if __name__ == "__main__":
     fig = plt.figure(figsize=(8,6))
     ax  = fig.add_subplot(111)
 
-
-
     fe1 = read_dat_gen("testAdaptive/pjiref.out")
     fe2 = read_dat_gen("testLBFGS/pjiref.out")
-    fe3 = read_dat_gen("testSparseSampling/pjiref.out")
-    ax.plot(fe1[:,0], fe1[:,-1], label="Adaptive", c='b')
-    ax.scatter(fe2[:,0], fe2[:,-1], label='LBFGS', c='r', s=60)
-    ax.scatter(fe3[:,0], fe3[:,-1], label='Sparse', c='g', s=60)
+    ax.plot(fe1[:,0], fe1[:,3], c='b',label='Adaptive')
+    ax.errorbar(fe1[:,0], fe1[:,3],fe1[:,4], c='b', fmt="o", linewidth=5, capsize=10)
+    ax.scatter(fe2[:,0], fe2[:,3], c='r', label='LBFGS', s=60)
 
     ax.set_xlabel(r"$\tilde{N}$")
     ax.set_ylabel(r"$\beta F$")
