366d023b2aa608f95d7f7f81b79babe4b086419a
Yusheng Cai <cys9741@seas.upenn.edu>
2022-05-05 16:54:06 -0400

    Updated plotKL

--- test/PlotCode/plotKL.py
+++ test/PlotCode/plotKL.py
@@ -1,18 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from analysis_code.lib.utils import read_dat_gen
+def ReadTabulatedData(file_path):
+    """
+    Function that reads the .dat from INDUS simulations
+
+    Args:
+        file_path(str): the path to the file 
+
+    Return:
+        an numpy array that contains the N and Ntilde from the INDUS simulation (N, Ntilde) -> where both are of shape (nobs,)
+    """
+    f = open(file_path)
+    lines = f.readlines()
+    lines = [line for line in lines if line[0]!="#"]
+
+    lines = [[float(num) for num in line.rstrip("\n").split()] for line in lines if line[0]!="#"]
+    lines = np.array(lines)
+
+    return lines
+
+
 
 if __name__ == "__main__":
-    folders = ["testAdaptive", "testLBFGS"]
-
     fig = plt.figure(figsize=(8,6))
     ax  = fig.add_subplot(111)
 
-    kl1 = read_dat_gen("testAdaptive/klref.out")
+    kl1 = ReadTabulatedData("../testAdaptive/klref.out")
     x = np.arange(1,len(kl1)+1)
 
-    ax.bar(x, kl1.flatten())
+    ax.bar(x, np.abs(kl1.flatten()))
     max_ =kl1.max() * 4
     ax.set_ylim(0,max_)
 
